d4807b3d35d36de94b4aa02bb48c598f4062310d
netty <netty.carroll@live.com>
2021-02-05 12:17:35 +0000

    js
    displaying keycodes

--- keycode/app.js
+++ keycode/app.js
@@ -2,4 +2,18 @@ const display = document.getElementById("display")
 
 window.addEventListener('keydown',(e)=>{
     console.log(e);
+    const key = e.key;
+    const keycode = e.keycode;
+    const code = e.code;
+    
+    display.innerHTML = `
+    <div class = "key">
+        ${key}
+    </div> 
+    <div class = "key">
+        ${keycode}
+    </div>
+    <div class ="key">
+        ${code}
+    </div>`
 })
